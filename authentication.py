6a2bb3100189f506dde4ff6022b9b9f8798458d9
mendoza1 <csmendoza@ucdavis.edu>
2018-06-11 00:18:46 -0700

    password history enforced, needs testing

--- authentication.py
+++ authentication.py
@@ -1,39 +1,46 @@
 from collections import deque
 
 # GLOBALS
-past_passwords = deque(["pass1", "jatelmuffin"]) 
+num_past_pass_allowed = 5
+past_passwords = deque(["pass1", "jatelmuffin"], num_past_pass_allowed) 
 
 def set_new_password(new_password):
 
 	#password has been used within last 5
-	if(used_n_before(new_password)):
-		return False
+	if(used_before(new_password)):
+		#PROMPT FOR OTHER PASSWORD
+		return False #dont allow 
 
-	return True
+	else: #track new password
 
-def used_n_before(new_password):
+		#max number of passwords being tracked
+		if(len(past_passwords) == num_past_pass_allowed):
+			past_passwords.popleft() #stop tracking oldest one
+
+		past_passwords.append(new_password)
+		return True
+	
+
+def used_before(new_password):
 	
 	# not used before
-	if(past_passwords.count(new_password) == 0):
-		past_passwords.append(new_password)
-
-	print past_passwords
-# 	//don't allow password
-
-# //else 
-# 	//pop queue, push queue
+	if(past_passwords.count(new_password) == 0):                     
+		return False
+	
 	return True
-
-# //enforce password history
-# 	//don't allow last 5
-
-# //in set new password
-# //keep passwords in queue
 
 
 # TIME TO BRING OUT MY ** MAIN **
-new_password = "gameandwatch"	
-set_new_password(new_password)
+new_passes = ["dude", "hey", "phone", "snookie", "other", "other"]
+
+for new_pass in new_passes:
+	
+	# failed to set new password
+	if(set_new_password(new_pass) == False):
+		print "the pass *", new_pass, "* has been used in the last ",num_past_pass_allowed
+
+
+print past_passwords
 
 
 
